86bc703c0fa64749a6c0b4a97fcc84fea73b3ab0
John Paul Bamberg <jpbamberg1993@gmail.com>
2017-09-06 12:22:26 -0400

    Remove logging

--- app/controller/redactor2_rails/images_controller.rb
+++ app/controller/redactor2_rails/images_controller.rb
@@ -6,19 +6,14 @@ class Redactor2Rails::ImagesController < ApplicationController
 
     file = params[:file]
     @image.data = Redactor2Rails::Http.normalize_param(file, request)
-    puts 'before attribute devise_user_key'
     if @image.has_attribute?(:"#{Redactor2Rails.devise_user_key}")
-      puts 'has attribute devise_user_key'
       @image.send("#{Redactor2Rails.devise_user}=", redactor2_current_user)
       @image.assetable = redactor2_current_user
     end
 
-    puts 'Before Save'
     if @image.save
-      puts 'SAVED!'
       render json: { id: @image.id, url: @image.url(:content) }
     else
-      puts 'NOT SAVED!'
       puts "Errors: #{@image.errors.messages}"
       render json: { error: @image.errors }
     end
@@ -27,7 +22,6 @@ class Redactor2Rails::ImagesController < ApplicationController
   private
 
   def redactor2_authenticate_user!
-    puts 'redactor2_authenticate_user!'
     if Redactor2Rails.image_model.new.has_attribute?(Redactor2Rails.devise_user)
       super
     end
